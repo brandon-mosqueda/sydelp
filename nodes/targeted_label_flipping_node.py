3b2bb9b06d5322553b9189c82b397bf0a0adb72a
Brandon Alejandro Mosqueda González <brandon-mosqueda@member.fsf.org>
2024-10-30 10:54:44 +0100

    * Correction targeted label flipping attack: Only the source label is changed.

--- nodes/targeted_label_flipping_node.py
+++ nodes/targeted_label_flipping_node.py
@@ -1,33 +1,27 @@
+import numpy as np
+
 from nodes.malicious_node import MaliciousNode
-from utils.attack import flip_labels
+from numpy.typing import NDArray
 
 
 # When attacking the model is normally trained but using the flipped labels
 class TargetedLabelFlippingNode(MaliciousNode):
-    has_real_labels: bool
-    target_label_1: int
-    target_label_2: int
+    source: int
+    target: int
+    source_indices: NDArray[np.int64]
 
     def __init__(self,
-                 target_label_1: int,
-                 target_label_2: int,
+                 source: int,
+                 target: int,
                  *args,
                  **kwargs) -> None:
         super().__init__(*args, **kwargs)
-        self.target_label_1 = target_label_1
-        self.target_label_2 = target_label_2
-        self.has_real_labels = True
+        self.source = source
+        self.target = target
+        self.source_indices = np.where(self.y == self.source)[0]
 
     def train(self) -> None:
-        # Attacking | Has_real | Change labels
-        #   False   | False    |    True        to original
-        #   False   | True     |    False
-        #   True    | False    |    False
-        #   True    | True     |    True        to flipped
-        if ((not self.attacking and not self.has_real_labels) or
-            (self.attacking and self.has_real_labels)):
-            flip_labels(self.y, self.target_label_1, self.target_label_2)
-
-        self.has_real_labels = not self.attacking
+        label: int = self.target if self.attacking else self.source
+        self.y[self.source_indices] = label
 
         super().train()
