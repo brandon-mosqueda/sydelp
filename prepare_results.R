6d8fae96eaec7e1022afb15650cd82156401c879
Brandon Alejandro Mosqueda González <brandon-mosqueda@member.fsf.org>
2024-11-07 10:21:46 +0100

    * First prepare results.

---
+++ prepare_results.R
@@ -0,0 +1,53 @@
+rm(list = ls())
+
+setwd("~/doctorado/experiments/decentralized_learning")
+
+library(tidyverse)
+library(ggbasic)
+library(SKM)
+
+source("~/Desktop/notes/utils.R")
+
+attack_levels <- c(
+  "No attack", "Random", "Sign flipping", "Solitary", "Label flipping"
+)
+Metrics <- merge_results("results", "/metrics.csv") %>%
+  mutate(
+    `log(loss)` = log10(loss),
+    Attack = factor(Attack, levels = attack_levels)
+  )
+write_csv(Metrics, "results/all_metrics.csv")
+
+plot_results <- function(data, group) {
+  plot <- line_plot(
+    data,
+    x = "round",
+    y = "Value",
+    facet_col = "Metric",
+    fill_by = "Protocol",
+    line_width = 0.5,
+    with_points = FALSE,
+    font_size = 8,
+    theme = "paper",
+    y_breaks_num = 5,
+    x_breaks_num = 5
+  )
+
+  plot_dir <- file.path("plots", group$Dataset)
+  mkdir(plot_dir)
+
+  ggsave(file.path(plot_dir, sprintf("%s.png", group$Attack)), plot)
+
+  return(1)
+}
+
+Metrics %>%
+  pivot_longer(
+    cols = c("accuracy", "f1_score", "attack_success_rate", "label_recall"),
+    names_to = "Metric",
+    values_to = "Value"
+  ) %>%
+  na.omit() %>%
+  droplevels() %>%
+  group_by(Dataset, Attack) %>%
+  group_map(plot_results, .keep = TRUE)
